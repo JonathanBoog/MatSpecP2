39b6f3cbcce63b9016cb58490c5601b4854bc987
NorrskenetPOG <gabril.nilsson@skola.taby.se>
2023-01-12 21:01:39 +0100

    update file

--- ovning_203/203.py
+++ ovning_203/203.py
@@ -1,17 +1,20 @@
 import numpy as np
 
+#Element
 x = np.array([150, 250, 450, 600, 725])
 y = np.array([74.3, 127, 230, 289, 367])
 
+#Skapar tomma arrayes (kallar dem matriser i detta fall)
 X, Y = np.empty((len(x), 2)), np.empty((len(y), 1))
 
+#Sätter in elemnten i matriserna
 X[:,0], X[:,1], Y[:,0] = 1, x, y
 
+#Ger oss bl.a en 2X2 vilket vi enkelt kan arbeta med
 XT_X, YT_Y = X.T@X, X.T@Y
 
-inv_X_T_X = np.linalg.inv(XT_X)
-
-B_B = inv_X_T_X@YT_Y
+#Skapar inversen till XT_X och multiplicerar på båda sidorna så vi får värderna för [B0, B1]    
+B_B = np.linalg.inv(XT_X)@YT_Y
 
 B0, B1 = B_B[0], B_B[1]
 
