2668fa2df8f202ab63850686d3bc053720887ca5
NorrskenetPOG <gabril.nilsson@skola.taby.se>
2023-01-12 17:40:47 +0100

    update file

--- ovning_203/203.py
+++ ovning_203/203.py
@@ -1,25 +1,18 @@
 import numpy as np
-from ctypes.wintypes import SIZE
 
-x = np.matrix([150, 250, 450, 600, 725])
-y = np.matrix([74.3, 127, 230, 289, 367])
-m, n = x.size, y.size
+x = np.array([150, 250, 450, 600, 725])
+y = np.array([74.3, 127, 230, 289, 367])
 
-X, Y = np.empty((m, 2)), np.empty((n, 1))
+X, Y = np.empty((len(x), 2)), np.empty((len(y), 1))
 
 X[:,0], X[:,1], Y[:,0] = 1, x, y
 
-X_T, Y_T = X.transpose(), Y.transpose()
-X_T_X, Y_T_Y = X_T@X, X_T@Y
+XT_X, YT_Y = X.T@X, X.T@Y
 
-inv_X_T_X = np.linalg.inv(X_T_X)
+inv_X_T_X = np.linalg.inv(XT_X)
 
-B_B = inv_X_T_X@Y_T_Y
+B_B = inv_X_T_X@YT_Y
 
 B0, B1 = B_B[0], B_B[1]
 
-print(f"""{B_B}, B0 = {B0} och B1 = {B1}
-
-
-y= {B1}x + {B0}
-""")
+print(f"""{B_B}, B0 = {B0} och B1 = {B1}""")
